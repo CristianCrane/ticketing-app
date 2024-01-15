e682e1455202238c17d07d07253f3ea02442efdc
Cris <12250662+CristianCrane@users.noreply.github.com>
2024-01-15 09:34:03 -0500

    add boilerplate for auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express'
-import { json } from 'body-parser'
+import {json} from 'body-parser'
+import {currentUserRouter} from "./routes/current-user";
+import {signUpRouter} from "./routes/sign-up";
+import {signInRouter} from "./routes/sign-in";
+import {signOutRouter} from "./routes/sign-out";
 
 const app = express()
 app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
- res.send('hi there')
-})
+app.use(currentUserRouter)
+app.use(signUpRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
 
 app.listen(3000, () => {
- console.log('Listening on port 3000')
+  console.log('Listening on port 3000')
 })
