94252ca2ade9efa7a4c4756f26c228579b7b7bd5
Cris <12250662+CristianCrane@users.noreply.github.com>
2024-01-17 09:17:19 -0500

    hash passwords

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import { model, Model, Document, Schema } from "mongoose";
+import { Password } from "../services/Password";
 
 /**
  * The properties required to create a new User.
@@ -43,6 +44,13 @@ const userSchema = new Schema<UserDoc, UserModel>({
 userSchema.statics.build = (fields: UserFields) => {
   return new User(fields);
 };
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
 
 const User = model<UserDoc, UserModel>("User", userSchema);
 
