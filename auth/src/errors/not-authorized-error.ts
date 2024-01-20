2651ca0fb08f3b0a7dc0776aa18956a2c6d74029
Cris <12250662+CristianCrane@users.noreply.github.com>
2024-01-20 09:49:06 -0500

    implement reusable require-auth middleware

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,13 @@
+import { BaseError } from "./BaseError";
+
+export class NotAuthorizedError extends BaseError {
+  statusCode = 401;
+
+  constructor() {
+    super("Not authorized");
+  }
+
+  serializeErrors(): { message: string; field?: string }[] {
+    return [{ message: this.message }];
+  }
+}
