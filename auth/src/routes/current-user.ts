37b908cd78a0d15aa660b7a3e79e1113f27c06cf
Cris <12250662+CristianCrane@users.noreply.github.com>
2024-01-20 08:38:37 -0500

    implement current user endpoint

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.status(200).send("currentuser");
+  const userJwt = req?.session?.jwt;
+
+  if (!userJwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(userJwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (e) {
+    // jwt was invalid/tampered with
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
