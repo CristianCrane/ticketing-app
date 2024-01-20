039c878bd4a336ab8a3f4c88c82c3988167ee98b
Cris <12250662+CristianCrane@users.noreply.github.com>
2024-01-20 09:32:05 -0500

    implement reusable currentUser middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,22 +1,10 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  const userJwt = req?.session?.jwt;
-
-  if (!userJwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(userJwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (e) {
-    // jwt was invalid/tampered with
-    return res.send({ currentUser: null });
-  }
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser ?? null });
 });
 
 export { router as currentUserRouter };
