9f5159e2896bea78de799d783ec9137ac0d2e403
Cris <12250662+CristianCrane@users.noreply.github.com>
2024-01-22 08:49:54 -0500

    setup mantine.dev for nextjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // todo: consider this option if you find HMR isnt working
+    // webpack: (config) => {
+    //     config.watchOptions.poll = 300
+    //     return config
+    // }
+};
 
 export default nextConfig;
